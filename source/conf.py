b451d679c03748bb2de1254027842db28e989e5e
Anton <anton.d.pravdin@gmail.com>
2025-08-20 12:00:39 +0300

    Добавил страницу по дисциплине Бизнес-медиация

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Бизнес-медиация'
+copyright = '2025, Anton Pravdin'
+author = 'Anton Pravdin'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+
+html_static_path = ['_static']
+
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+
